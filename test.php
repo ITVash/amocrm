fa7a3d54eeea57dfbcd47f174b738ee0ce888e98
ITVash <vash_dn@mail.ru>
2019-07-05 23:18:10 +0300

    Fix

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  require_once 'lead.php';
-  use Leads\Lead;
-  $lead = new Lead('max.zoll45@yandex.ru', 'de4da9f0a169dcfafdf97d50ab8890db', 'dnevnikarma');
-  //$auth = $lead->test();
-
-  $list = $lead->list();
-  
-?>
